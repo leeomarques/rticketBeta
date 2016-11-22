f06067a0eca9faa4accb4b3f09ec849357649ae5
toinhotony <acorreaoliveira@gmail.com>
2016-11-22 17:35:33 -0300

    Atualizar Perfil

--- rticketWeb/src/java/com/rticket/beans/PerfilBean.java
+++ rticketWeb/src/java/com/rticket/beans/PerfilBean.java
@@ -8,26 +8,54 @@ import com.rticket.model.Perfil;
 import com.rticket.negocio.Fachada;
 import com.rticket.negocio.IFachada;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
+import org.primefaces.model.DualListModel;
 
 @ManagedBean(name = "perfilBean")
 public class PerfilBean {
 
-    private String nome;
+    private DualListModel<String> dualListModulo;
     private Collection<Modulo> listarModulo;
     private Collection<Perfil> listarPerfil;
+    private String nome;
     private Perfil perfil;
     private Modulo modulo;
 
     IFachada fach = new Fachada();
 
+    @PostConstruct
+    public void init() {
+    
+        List<String> moduloSource = new ArrayList();
+        List<String> moduloTarget = new ArrayList();
+        
+        Iterator<Modulo> iterator;
+        
+        iterator = fach.listarModulo().iterator();
+        
+        while(iterator.hasNext()) {
+            Modulo item = (Modulo)iterator.next();
+            moduloSource.add(item.getNome());
+         }       
+         
+        dualListModulo = new DualListModel<String>(moduloSource, moduloTarget);        
+    }
+    
     public void inserirPerfil() throws CampoExistenteException, FormatoInvalidoException, CampoVazioException {
         Perfil per = new Perfil();
         per.setNome(nome);
+  
         fach.inserirPerfil(per);
         try {
             FacesContext.getCurrentInstance().getExternalContext().redirect("perfil.xhtml");
@@ -43,13 +71,13 @@ public class PerfilBean {
     public void alterarPerfil(Perfil perfil) {
         fach.alterarPerfil(perfil);
     }
-
-    public String getNome() {
-        return nome;
+    
+    public DualListModel<String> getDualListModulo() {
+        return dualListModulo;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setDualListModulo(DualListModel<String> dualListModulo) {
+        this.dualListModulo = dualListModulo;
     }
 
     public Collection<Modulo> getListarModulo() {
@@ -60,7 +88,23 @@ public class PerfilBean {
     public void setListarModulo(Collection<Modulo> listarModulo) {
         this.listarModulo = listarModulo;
     }
+    
+    public Collection<Perfil> getListarPerfil() {
+        return fach.listarPerfil();
+    }
 
+    public void setListarPerfil(Collection<Perfil> listarPerfil) {
+        this.listarPerfil = listarPerfil;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
     public Perfil getPerfil() {
         return perfil;
     }
@@ -76,13 +120,19 @@ public class PerfilBean {
     public void setModulo(Modulo modulo) {
         this.modulo = modulo;
     }
-
-    public Collection<Perfil> getListarPerfil() {
-        return fach.listarPerfil();
+    
+    public void onSelect(SelectEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Selected", event.getObject().toString()));
     }
-
-    public void setListarPerfil(Collection<Perfil> listarPerfil) {
-        this.listarPerfil = listarPerfil;
+     
+    public void onUnselect(UnselectEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Unselected", event.getObject().toString()));
     }
-
+     
+    public void onReorder() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "List Reordered", null));
+    } 
 }
